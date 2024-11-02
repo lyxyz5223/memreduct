57a9d470b97be394e9815402b058a09bd16517ca
裂（*＾-＾*） <ly1620682458@outlook.com>
2024-11-02 13:29:23 +0800

    更新文件信息

--- src/app.h
+++ src/app.h
@@ -7,5 +7,5 @@
 #define APP_NAME_SHORT L"memreduct"
 #define APP_VERSION L"3.4.1"
 #define APP_VERSION_RES 3,4,1,0
-#define APP_AUTHOR L"Henry++"
+#define APP_AUTHOR L"Henry++(Modified by lyxyz5223)"
 #define APP_COPYRIGHT L"(c) 2011-2022 " APP_AUTHOR L". All Rights Reversed."
